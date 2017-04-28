7819460bad37a596af7f50200dc931d2a8cf30bb
keyhit <viacheslavkarpenkoju@gmail.com>
2017-04-28 23:22:47 +0300

    Added departaments list and units list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,20 @@ class ApplicationController < ActionController::Base
     def organization
       @organization ||= Organization.find(params[:organization_id])
     end
-
     helper_method :organization
 
     def departament
       @departament ||= organization.departaments.find(params[:departament_id])
     end
-
     helper_method :departament
 
+    def departaments
+      @departaments ||= organization.departaments.where(params[:organization_id])
+    end
+    helper_method :departaments
+
+    def units
+      @units ||=departament.units.where(params[:departament_id])
+    end
+    helper_method :units
 end
