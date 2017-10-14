b5cb63fe44c29258dd3e6407b0c38c6f5ac65502
keyhit <viacheslavkarpenkoju@gmail.com>
2017-10-14 23:47:45 +0300

    ver1.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
 
   config.action_mailer.smtp_settings = {
@@ -100,3 +100,4 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
 end
+ 
